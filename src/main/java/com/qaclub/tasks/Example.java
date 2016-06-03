99e0199c60d306a4daba38f304675bffa05d45eb
Alex Galkin <alex.galkin@geeksforless.com>
2016-06-03 12:21:50 +0300

    Added initial files for repo with example task

---
+++ src/main/java/com/qaclub/tasks/Example.java
@@ -0,0 +1,15 @@
+package com.qaclub.tasks;
+
+public class Example {
+
+	public static void main(String[] args) {
+		Student alex = new Student();
+		
+		alex.setName("Alex Galkin");
+		alex.setScore(72);
+		
+		System.out.println("Student: " + alex.getName()
+				+ " |  Score: " + alex.getScore());
+	}
+
+}
